859bd430c00d93b3e49f61d7cb4dce3161b55adb
Usama Qureshi <u26.qureshi@gmail.com>
2024-02-14 22:13:53 +0500

    feat: respond to ping

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import socket
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    connection, address = server_socket.accept()
+    with connection:
+        data = connection.recv(4096)
+        connection.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
